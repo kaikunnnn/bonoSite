2b7cf491117909fea33cf3cf07d4ead3bafc5e49
kaikunnnn <kyasya00@gmail.com>
2023-08-29 09:44:26 +0900

    Rename file to temp.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge:["./src/**/*.{js,ts,jsx,tsx}","./pages/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
+  content:["./src/**/*.{js,ts,jsx,tsx}","./pages/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
   theme: { 
     extend: {
       fontSize:{
