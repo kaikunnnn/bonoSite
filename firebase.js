d57d029f7ee10495af278b9201102994aeb30dd8
kaikunnnn <kyasya00@gmail.com>
2023-08-31 08:55:16 +0900

    Fix the variables.

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA61sGj5t6d4qY5uubC--Ns-n4sTbiueQE",
-  authDomain: "bono-fc42f.firebaseapp.com",
-  projectId: "bono-fc42f",
-  storageBucket: "bono-fc42f.appspot.com",
-  messagingSenderId: "637812170207",
-  appId: "1:637812170207:web:989cc111ed4fd79cd8b86a",
-  measurementId: "G-CFJZN8L7LE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
