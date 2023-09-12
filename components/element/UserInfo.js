9f343f5139f7e8fc5f6b23af3e791bbda837f2b1
kaikunnnn <kyasya00@gmail.com>
2023-09-12 22:26:35 +0900

    Change the profile page styles.

--- components/element/UserInfo.js
+++ components/element/UserInfo.js
@@ -1,18 +1,40 @@
 import React from "react";
-import {auth} from "../../firebase";
-
+import { auth } from "../../firebase";
+import List from "./object/list";
 
 function UserInfo() {
-    
-    return(
-        <div>
-            <img src={auth.currentUser.photoURL} alt={auth.currentUser.displayName} />
-            <p>{auth.currentUser.displayName}</p>
-            <p>{auth.currentUser.email}</p>
-            
+  return (
+    <div>
+      <img src={auth.currentUser.photoURL} alt={auth.currentUser.displayName} />
+      <p>{auth.currentUser.displayName}</p>
+      <p>{auth.currentUser.email}</p>
+      <div data-ms-content="members">
+        <div class="title-blcok">
+          <div class="textblock-left">
+            <div class="heading-h5">アカウント設定</div>
+            <div class="margin-8 hide"></div>
+          </div>
+          <a href="#" class="buttonsecondaryghost medium hide w-inline-block">
+            <div class="body-medium white">変更する</div>
+          </a>
         </div>
-        
-    )
+        <div class="mt-4"></div>
+        <List
+            label="メールアドレスの変更"
+            content="takumi.kai.skywalker@gmail.com"
+            buttonLabel="変更"
+            buttonLink="/"
+        ></List>
+        <div class="mt-4"></div>
+        <List
+            label="パスワードの変更"
+            content="ボタン→Change Password"
+            buttonLabel="変更"
+            buttonLink="/"
+        ></List>
+      </div>
+    </div>
+  );
 }
 
-export default UserInfo
+export default UserInfo;
