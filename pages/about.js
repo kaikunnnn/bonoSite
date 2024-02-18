0a87cf3c0a3864b626f42ca63425824f39a754ea
kaikunnnn <kyasya00@gmail.com>
2024-02-18 18:50:19 +0900

    OGP設定

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,7 @@ import Story from "@/components/About/layout/Story";
 import Stance from "@/components/About/layout/Stance";
 import Header from "@/components/common/layout/Navigation/Header";
 import { getContents } from "@/libs/newt"; // getContents関数をインポート
+import SEO from "@/components/common/layout/Navigation/SEO";
 
 export async function getStaticProps() {
   const contents = await getContents();
@@ -21,6 +22,16 @@ function About({aboutContents}) {
   
   return (
     <>
+    <SEO
+      title={`${article.title} | BONO BLOG`}
+      description={article.description}
+      imgUrl={`${article.emoji.src}`}
+      ogTitle={`${article.title} | BONO BLOG`}
+      ogImage={article.emoji.src}
+      ogDescription={article.description}
+      ogWidth='160'
+      ogHeight="160"
+    ></SEO>
     <main className="max-h-full  text-slate-900">
       <Header />
       <div className="w-full">
