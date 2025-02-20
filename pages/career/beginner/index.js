c15eb44d7c9cc0d8349a467e48811127e2038cd7
kaikunnnn <kyasya00@gmail.com>
2025-02-20 13:29:10 +0900

    CareerBeginerのEyecatch実装

--- pages/career/beginner/index.js
+++ pages/career/beginner/index.js
@@ -5,9 +5,8 @@ import ExampleOfUiuxdesignerFromScratch from "@/components/feature/career/Exampl
 import RelatedContentSection from "@/components/feature/career/MoreSection";
 export default function HomePage() {
   return (
-    <div className="bg-white">
+    <div className="bg-stone-100">
       <CareerHeroSection />
-      <CourseSectionUIUX />
       <div className="Articles-Section justify-center items-center">
         <div className="Articles-Section-Title"></div>
         <ArticleSection />
@@ -16,8 +15,9 @@ export default function HomePage() {
       <ExampleOfUiuxdesignerFromScratch />
       <div className="w-full h-[200px] p-2.5 bg-[#fff6fd] border-t border-b border-black flex-col justify-center items-center gap-2.5 inline-flex">
         <div className="w-32 text-center text-[#1e1e1e] text-sm font-bold font-['Noto Sans JP'] leading-[18.48px]">旅のことば</div>
-        <div className="text-center text-[#1e1e1e] text-base font-bold font-['Noto Sans JP'] leading-[21.12px]">“デザインはオリジナリティを発揮するもの。と思い込んではいけません。<br/>まずは世の中の良いものに目を向けて”定番”の表現を吸収していくのです”</div>
+        <div className="text-center text-[#1e1e1e] text-base font-bold font-['Noto Sans JP'] leading-[21.12px]">“デザインはオリジナリティを発揮するもの。と思い込んではいけません。<br />まずは世の中の良いものに目を向けて”定番”の表現を吸収していくのです”</div>
       </div>
+      <CourseSectionUIUX />
       <RelatedContentSection />
     </div>
   );
